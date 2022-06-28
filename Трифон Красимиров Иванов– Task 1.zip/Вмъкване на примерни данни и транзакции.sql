abc55ac2965589d499885741f76b0038ad37dfba
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-28 10:08:48 +0300

    Make [PHONE_TYPES] table [PHONE_TYPE] column unique

--- Трифон Красимиров Иванов– Task 1.zip/Вмъкване на примерни данни и транзакции.sql
+++ Трифон Красимиров Иванов– Task 1.zip/Вмъкване на примерни данни и транзакции.sql
@@ -2,34 +2,34 @@ USE [PhoneBook]
 
 BEGIN TRANSACTION
 
-INSERT INTO [CITIES]
-	([CITY_NAME],[AREA_NAME],[POSTAL_CODE])
-	VALUES
-	('Бургас','Бургас',8000),
-	('Пловдив', 'Пловдив',4000),	
-	('Варна','Варна',9000),
-	('София','София',1000);
+	INSERT INTO [CITIES]
+		([CITY_NAME],[AREA_NAME],[POSTAL_CODE])
+		VALUES
+		('Бургас','Бургас',8000),
+		('Пловдив', 'Пловдив',4000),	
+		('Варна','Варна',9000),
+		('София','София',1000);
 
-INSERT INTO [PERSONS]
-	([FIRST_NAME],[SECOND_NAME],[LAST_NAME],[CITY_ID],[ADDRESS])
-	VALUES
-	('Трифон','Красимиров','Иванов',1,'Жк.Възраждане/Ул.Цар Калоян №24'),
-	('Христо','Стоянов','Петров',2,'Жк.Тракия/Ул.Лозенград №3'),
-	('Георги','Ангелов','Анастасов',1,'Жк.Лазур/Ул.Христо Ботев №9'),
-	('Трифон','Красимиров','Иванов',4,'Жк.Студентски град/Ул.Парижка комуна №8')
+	INSERT INTO [PERSONS]
+		([FIRST_NAME],[SECOND_NAME],[LAST_NAME],[CITY_ID],[ADDRESS])
+		VALUES
+		('Трифон','Красимиров','Иванов',1,'Жк.Възраждане/Ул.Цар Калоян №24'),
+		('Христо','Стоянов','Петров',2,'Жк.Тракия/Ул.Лозенград №3'),
+		('Георги','Ангелов','Анастасов',1,'Жк.Лазур/Ул.Христо Ботев №9'),
+		('Трифон','Красимиров','Иванов',4,'Жк.Студентски град/Ул.Парижка комуна №8')
 
+	INSERT INTO [PHONE_TYPES] 
+		(PHONE_TYPE) VALUES
+		('Home'),
+		('Mobile'),
+		('Work')
 
-INSERT INTO [PHONE_TYPES] 
-	(PHONE_TYPE) VALUES
-	('Home'),
-	('Mobile'),
-	('Work')
+	INSERT INTO [PHONE_NUMBERS]
+		([PERSON_ID],[PHONE_NUMBER],[PHONE_TYPE_ID])
+		VALUES
+		(1,'0893668829',2),
+		(2,'0899628177',3),
+		(3,'0876351840',1),
+		(4,'0882183197',2)
 
-INSERT INTO [PHONE_NUMBERS]
-	([PERSON_ID],[PHONE_NUMBER],[PHONE_TYPE_ID])
-	VALUES
-	(1,'0893668829',2),
-	(2,'0899628177',3),
-	(3,'0876351840',1),
-	(4,'0882183197',2)
 COMMIT TRANSACTION
