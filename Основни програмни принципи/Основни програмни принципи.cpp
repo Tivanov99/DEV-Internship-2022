062c231198e4432dc92246a090df6f6671e531a0
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-28 20:35:39 +0300

    Work with pointers

--- Основни програмни принципи/Основни програмни принципи.cpp
+++ Основни програмни принципи/Основни програмни принципи.cpp
@@ -2,19 +2,27 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int v = 10;
+
+	int* y;
+	y = &v;
+	cout << sizeof(*y) << "\n";
+	y + 5;
+	cout << sizeof(*y) << "\n";
+
+
+	int arr[5] = { 1,2,3,4,5 };
+	int* p = arr;
+
+	for (size_t i = 0; i < 5; i++)
+	{
+		cout << *p << "\n";
+		p++;
+	}
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
