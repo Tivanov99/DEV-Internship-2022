2921aa04cb145d0368cb5f7365875cb0afaedc2e
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-30 11:28:47 +0300

    Update

--- Трифон Красимиров Иванов– Task 1/Създаване на базата данни.sql
+++ Трифон Красимиров Иванов– Task 1/Създаване на базата данни.sql
@@ -1,1 +1,21 @@
 CREATE DATABASE [PhoneBook]
+ON PRIMARY
+  ( NAME='PhoneBook_Primary',
+    FILENAME=
+       'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\PhoneBook_Prm.mdf',
+    SIZE=4MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB),
+FILEGROUP MyDB_FG1
+  ( NAME = 'PhoneBook_FG1_Dat1',
+    FILENAME =
+       'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\PhoneBook_FG1_Dat1.mdf',
+    SIZE = 1MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB),
+  ( NAME = 'PhoneBook_FG1_Dat2',
+    FILENAME =
+	'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\PhoneBook_FG1_2.ndf',
+    SIZE = 1MB,
+    MAXSIZE=10MB,
+    FILEGROWTH=1MB)
