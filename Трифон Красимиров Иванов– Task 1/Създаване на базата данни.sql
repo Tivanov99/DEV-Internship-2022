72de91fa37dac87b7764f12dfda7003efae85e1a
Tivanov99 <trifon.ivanov21@gmail.com>
2022-06-30 12:07:05 +0300

    Update db

--- Трифон Красимиров Иванов– Task 1/Създаване на базата данни.sql
+++ Трифон Красимиров Иванов– Task 1/Създаване на базата данни.sql
@@ -1,21 +1,27 @@
+declare @Path varchar(100)
+set @Path = 'C:\Users\Public\Desktop\DATA'
+EXEC master.dbo.xp_create_subdir @Path
+
+DROP DATABASE IF EXISTS [PhoneBook]
+
 CREATE DATABASE [PhoneBook]
 ON PRIMARY
   ( NAME='PhoneBook_Primary',
     FILENAME=
-       'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\PhoneBook_Prm.mdf',
-    SIZE=4MB,
-    MAXSIZE=10MB,
+       'C:\Users\Public\Desktop\DATA\PhoneBook_Prm.mdf',
+    SIZE=500MB,
+    MAXSIZE=700MB,
     FILEGROWTH=1MB),
 FILEGROUP MyDB_FG1
   ( NAME = 'PhoneBook_FG1_Dat1',
     FILENAME =
-       'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\PhoneBook_FG1_Dat1.mdf',
+       'C:\Users\Public\Desktop\DATA\PhoneBook_FG1_Dat1.mdf',
     SIZE = 1MB,
     MAXSIZE=10MB,
     FILEGROWTH=1MB),
   ( NAME = 'PhoneBook_FG1_Dat2',
     FILENAME =
-	'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\PhoneBook_FG1_2.ndf',
+	'C:\Users\Public\Desktop\DATA\PhoneBook_FG1_2.ndf',
     SIZE = 1MB,
     MAXSIZE=10MB,
     FILEGROWTH=1MB)
