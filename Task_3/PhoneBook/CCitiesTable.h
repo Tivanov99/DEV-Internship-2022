b77938a0c61af2356a2877ffb1b88920c6db32b1
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-04 14:04:28 +0300

    Added Structures.h filed witch contains all disk structures

--- Task_3/PhoneBook/CCitiesTable.h
+++ Task_3/PhoneBook/CCitiesTable.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Structures.h"
 class CCitiesTable
 {
 public:
@@ -6,5 +7,10 @@ public:
 	{
 			
 	}
+	BOOL SelectAll(CCitiesArray& oCitiesArray);
+	BOOL SelectWhereID(const long lID, CITIES& recCities);
+	BOOL UpdateWhereID(const long lID, const CITIES& recCities);
+	BOOL Insert(const CITIES& recCities);
+	BOOL DeleteWhereID(const long lID);
 };
 
