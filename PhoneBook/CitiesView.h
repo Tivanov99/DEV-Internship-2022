f80710c51a14d894231672105eae9da3b43b7714
Trifon Krasimirov Ivanov <trifon.ivanov21@gmail.com>
2022-07-04 15:31:53 +0300

    Add files via upload

---
+++ PhoneBook/CitiesView.h
@@ -0,0 +1,49 @@
+
+// CitiesView.h : interface of the CCitiesView class
+//
+
+#pragma once
+
+
+class CCitiesView : public CListView
+{
+protected: // create from serialization only
+	CCitiesView() noexcept;
+	DECLARE_DYNCREATE(CCitiesView)
+
+// Attributes
+public:
+	CCitiesDoc* GetDocument() const;
+
+// Operations
+public:
+
+// Overrides
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void OnInitialUpdate(); // called first time after construct
+
+// Implementation
+public:
+	virtual ~CCitiesView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // debug version in CitiesView.cpp
+inline CCitiesDoc* CCitiesView::GetDocument() const
+   { return reinterpret_cast<CCitiesDoc*>(m_pDocument); }
+#endif
+
