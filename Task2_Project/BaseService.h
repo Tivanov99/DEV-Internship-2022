7270cd00e431ecc866321d2427f952d385b803af
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-07 23:27:43 +0300

    Remove ValidateIndex from BaseService.h

--- Task2_Project/BaseService.h
+++ Task2_Project/BaseService.h
@@ -20,8 +20,6 @@ public:
 	{
 
 	}
-	//<summary>функция валидираща диапазона на nIndex и максимален индекс nLenght.</summary>
-	void ValidateIndex(const int nIndex, const int nLenght);
 	//<summary>Функция добавяща всички дефоутни елементи в подаден масив като аргумент.</summary>
 	virtual void AddDefaultElements(CTypeArray &oArray) = 0;
 	//<summary>Функция принтираща информация за обект намиращ се на позицията на nIndex, всеки под сървис я импелеметира.</summary>
@@ -30,19 +28,6 @@ public:
 	T* GetPointerAtIndex(const int nIndex, CTypeArray &oArray);
 	//<summary>Функция премахваща определен брой Pointer-и 'count'започваща от подаден като аргумент индес 'nIndex'</summary>
 	void  RemoveAt(const int nIndex, const int count, CTypeArray &oArray);
-};
-
-template<class T>
-void BaseService<T>::ValidateIndex(const int nIndex, const int nLenght)
-{
-	if (nIndex > nLenght)
-	{
-		throw invalid_argument("Index out of range!");
-	}
-	else if (nIndex < 0)
-	{
-		throw invalid_argument("Index should be possitive!");
-	}
 };
 
 template<class  T>
