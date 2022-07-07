8fdd0fc9cd5574131e0015629df34514b6e44b8e
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-08 00:03:50 +0300

    Update

--- Task2_Project/BaseService.h
+++ Task2_Project/BaseService.h
@@ -4,7 +4,7 @@
 using namespace std;
 //<summary>Базов template-ен клас за всики сървиси</summary>
 template<class  T>
-class BaseService 
+class BaseService
 {
 
 private:
@@ -21,29 +21,26 @@ public:
 
 	}
 	//<summary>Функция добавяща всички дефоутни елементи в подаден масив като аргумент.</summary>
-	virtual void AddDefaultElements(CTypeArray &oArray) = 0;
+	virtual void AddDefaultElements(CTypeArray& oArray) = 0;
 	//<summary>Функция принтираща информация за обект намиращ се на позицията на nIndex, всеки под сървис я импелеметира.</summary>
-	virtual void ShowElementInfoAtIndex(const int nIndex, const CTypeArray &oArray) = 0;
+	virtual void ShowElementInfoAtIndex(const int nIndex, const CTypeArray& oArray) = 0;
 	//<summary>Функция връщаща Pointer от подаден индекс като аргумент.</summary>
-	T* GetPointerAtIndex(const int nIndex, CTypeArray &oArray);
+	T* GetPointerAtIndex(const int nIndex, CTypeArray& oArray);
 	//<summary>Функция премахваща определен брой Pointer-и 'count'започваща от подаден като аргумент индес 'nIndex'</summary>
-	void  RemoveAt(const int nIndex, const int count, CTypeArray &oArray);
+	void  RemoveAt(const int nIndex, const int count, CTypeArray& oArray);
 };
 
 template<class  T>
 T* BaseService<T>::GetPointerAtIndex(const int nIndex, CTypeArray& oArray)
 {
-	try
-	{
-		ValidateIndex(nIndex, oArray.GetCount() - 1);
-		T* pPointer = NULL;
-		pPointer = oArray[nIndex];
-		return pPointer;
-	}
-	catch (exception ex)
-	{
-		cout << ex.what() << endl;
-	}
+	if (nIndex > oArray.GetCount() - 1)
+		return NULL;
+
+	T* pPointer = oArray.GetAt(nIndex);
+	if(pPointer==NULL)
+		return NULL;
+
+	return pPointer;
 };
 
 template<class  T>
