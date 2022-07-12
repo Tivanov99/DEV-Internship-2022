cb74539c009af7ce465e65f25c13eacdbbfa4b9c
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-12 08:32:53 +0300

    Added methods description to PhoneNumberService.h

--- Task2_Project/PhoneNumberService.h
+++ Task2_Project/PhoneNumberService.h
@@ -8,8 +8,12 @@ using namespace std;
 class PhoneNumberService :public BaseService<PHONE_NUMBERS>
 {
 private:
-	//<summary>Попълва 'PHONE_NUMBER' на обект подаден като аргумент 'phoneNumber'.</summary>
-	void SetPhoneNumber(const CString& sorce, PHONE_NUMBERS& phoneNumber);
+	///<summary>
+		/// Попълва 'PHONE_NUMBER' със стойността 'oPhoneNumber' на обект подаден като аргумент 'phoneNumber'.
+	/// </summary>
+	/// <param name="strPhoneNumber">Телефонен номер.</param>
+	/// <param name="oPhoneNumber">Референция към обект.</param>
+	void SetPhoneNumber(const CString& strPhoneNumber, PHONE_NUMBERS& oPhoneNumber);
 public:
 
 	PhoneNumberService()
@@ -19,15 +23,36 @@ public:
 	~PhoneNumberService() {
 
 	}
-	//<summary>Функция добавяща всички дефоутни елементи в подаден масив
-	//като аргумент пренаписана от базов клас 'BaseService'.</summary>
-	void AddDefaultElements(CPhoneNumbersArray& oArray) override;
-	//<summary>Функция принтираща информация за обект намиращ се на позицията на nIndex,
-	//всеки под сървис я импелеметира пренаписана от базов клас 'BaseService'.</summary>
+	
+	///<summary>
+		//Функция променяща стойността на 'PHONE_NUMBER' на обект чиято текуща сойност е 'oldPhoneNumber'.
+	///</summary>
+	/// <param name="strCurrentPhoneNumber">Текущ телефонен номер.</param>
+	/// <param name="strNewPhoneNumber">Нов телефонен номер.</param>
+	void ChangePhoneNumber(const CString &strCurrentPhoneNumber, const CString &strNewPhoneNumber,
+		CPhoneNumbersArray& oPhoneTypesArray);
+
+
+	///<summary>
+		/// Функция променяща стойността на 'PHONE_NUMBER' на обект подаден като аргумент 'pOldPhoneNumberElement' .
+	/// </summary>
+	/// <param name="оPhoneNumber">Референция към обект.</param>
+	/// <param name="strNewPhoneNumber">Нов телефонен номер.</param>
+	void ChangePhoneNumber(PHONE_NUMBERS& оPhoneNumber, const CString &strNewPhoneNumber);
+	
+	///<summary>
+		///Функция принтираща информация за обект намиращ се на позицията на nIndex.
+		///Функция пренаписана от базов клас 'BaseService'.
+	///</summary>
+	/// <param name="оnIndex">Позиция на елемента в масива.</param>
+	/// <param name="оoArray">Масив с елементи.</param>
 	void ShowElementInfoAtIndex(const int nIndex, const CPhoneNumbersArray& oArray) override;
-	//<summary>Функция променяща стойността на 'PHONE_NUMBER' на обект чиято текуща сойност е 'oldPhoneNumber'.</summary>
-	void ChangePhoneNumber(const CString &strPhoneNumber, const CString &strNewPhoneNumber, CPhoneNumbersArray& oPhoneTypesArray);
-	//<summary>Функция променяща стойността на 'PHONE_NUMBER' на обект подаден като аргумент 'pOldPhoneNumberElement' .</summary>
-	void ChangePhoneNumber(PHONE_NUMBERS& pOldPhoneNumberElement, const CString &strNewPhoneNumber);
+	
+	///<summary>
+		/// Функция добавяща всички дефоутни елементи в подаден масив като аргумент.
+		/// Функция пренаписана от базов клас 'BaseService'.
+	/// </summary>
+	///<param name="citiesArray">Масив в който ще се добавят всички стойности.</param>
+	void AddDefaultElements(CPhoneNumbersArray& citiesArray) override;
 };
 
