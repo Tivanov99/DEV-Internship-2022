f956c77477944677ab41a9b958761d6a6435dffa
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-12 09:32:34 +0300

    Added description to new methods

--- Task2_Project/CityService.h
+++ Task2_Project/CityService.h
@@ -8,18 +8,18 @@ class CityService : public BaseService<CITIES>
 {
 private:
 	///<summary>Функция задаваща сойност на 'CITY_NAME' на подаден като аругмент показател</summary>
-	/// <param name="strCityName">Името на град което ще бъде зададено като стойност</param>
-	/// <param name="oCity">Обект чието "szCITY_NAME" ще бъде зададено с подадената стойност от "strCityName"</param>
-	void SetCityName(const CString &strCityName, CITIES& oCity);
+	///<param name="strCityName">Името на град което ще бъде зададено като стойност</param>
+	///<param name="oCity">Обект чието "szCITY_NAME" ще бъде зададено с подадената стойност от "strCityName"</param>
+	void SetCityName(const CString& strCityName, CITIES& oCity);
 
 	///<summary>Функция задаваща сойност на 'AREA_NAME' на подаден като аругмент показател</summary>
-	/// <param name="strAreaName">Името на област което ще бъде зададено като стойност</param>
-	/// <param name="oCity"> Името на околност което ще бъде зададено като стойност на "szAREA_NAME"</param>
-	void SetAreaName(const CString &strAreaName, CITIES& oCity);
+	///<param name="strAreaName">Името на област което ще бъде зададено като стойност</param>
+	///<param name="oCity"> Името на околност което ще бъде зададено като стойност на "szAREA_NAME"</param>
+	void SetAreaName(const CString& strAreaName, CITIES& oCity);
 
 	///<summary>Функция задаваща сойност на 'POSTAL_CODE' на подаден като аругмент показател</summary>
-	/// <param name="nPostalCode">Пощенски код който ще бъде зададен като стойност</param>
-	/// <param name="oCity">Името на околност което ще бъде зададено като стойност на "lPOSTAL_CODE"</param>
+	///<param name="nPostalCode">Пощенски код който ще бъде зададен като стойност</param>
+	///<param name="oCity">Името на околност което ще бъде зададено като стойност на "lPOSTAL_CODE"</param>
 	void SetPostalCode(const int nPostalCode, CITIES& oCity);
 
 	CCitiesArray oCities;
@@ -27,54 +27,72 @@ public:
 	CityService();
 	~CityService();
 
+	///<summary>Функция връщата указател като резултат която търси обект по име на област'strCityAreaName'.</summary>
+	///<param name="strCityAreaName">Име на област</param>
 	CITIES* GetByAreaName(const CString& strCityAreaName);
 
+	///<summary>Функция връщата указател като резултат която търси обект по име на град'strCityName'.</summary>
+	///<param name="strCityName">Име на град</param>
 	CITIES* GetByCityName(const CString& strCityName);
 
+	///<summary>Функция връщата указател като резултат която търси обект по пощенски код 'lPostalCode'.</summary>
+	///<param name="lPostalCode">Пощенски код</param>
 	CITIES* GetByPostalCode(const long lPostalCode);
-	
-	///<summary>Функция променяща 'CITY_NAME' стойността на обект чието текущо име е подадено като аргумент 'strCurrentCityName'.</summary>
-	/// <param name="strCurrentCityName">Текущото име на градът.</param>
-	/// <param name="strNewCityName">Новото име на градът.</param>
-	/// <param name="oCitiesArray">Масивът в който ще се търси град по подаденото текущо име "strCurrentCityName".</param>
-	void ChangeCityName(const CString &strCurrentCityName, const CString &strNewCityName);
+
+	///<summary>
+		/// Функция променяща 'CITY_NAME' стойността на обект
+		///  чието текущо име е подадено като аргумент 'strCurrentCityName'.
+	///</summary>
+	///<param name="strCurrentCityName">Текущото име на градът.</param>
+	///<param name="strNewCityName">Новото име на градът.</param>
+	///<param name="oCitiesArray">Масивът в който ще се търси град по подаденото текущо име "strCurrentCityName".</param>
+	void ChangeCityName(const CString& strCurrentCityName, const CString& strNewCityName);
 
 	///<summary>Функция променяща 'CITY_NAME' на обект подаден като аргумент.</summary>
-	/// <param name="оCity">Референция към обект.</param>
-	/// <param name="newCityName">Новото име на градът.</param>
-	void ChangeCityName(CITIES& оCity, const CString &newCityName);
+	///<param name="оCity">Референция към обект.</param>
+	///<param name="newCityName">Новото име на градът.</param>
+	void ChangeCityName(CITIES& оCity, const CString& newCityName);
 
-	///<summary>Функция променяща 'AREA_NAME' стойността на обект чието текущо име е подадено като аргумент 'oldCityAreaName'.</summary>
-	/// <param name="newCityName">Текущото име на областта.</param>
-	/// <param name="strNewCityAreaName">Новото име на областта.</param>
-	/// <param name="oCitiesArray">Масив в който ще се търси обект по стойността "strCurrentCityAreaName".</param>
-	void ChangeCityAreaName(const CString &strCurrentCityAreaName, const CString &strNewCityAreaName);
+	///<summary>
+		/// Функция променяща 'AREA_NAME' стойността на обект
+		///чието текущо име е подадено като аргумент 'strCurrentCityAreaName'.
+	///</summary>
+	///<param name="newCityName">Текущото име на областта.</param>
+	///<param name="strNewCityAreaName">Новото име на областта.</param>
+	///<param name="oCitiesArray">Масив в който ще се търси обект по стойността "strCurrentCityAreaName".</param>
+	void ChangeCityAreaName(const CString& strCurrentCityAreaName, const CString& strNewCityAreaName);
 
 	///<summary>Функция променяща 'AREA_NAME' на обект подаден като аргумент.</summary>
-	/// <param name="oCity">Референция към обект.</param>
-	/// <param name="strNewAreaName">Стойност с която ще бъде подменена текущата намираща се в "szAREA_NAME".</param>
-	void ChangeCityAreaName(CITIES& oCity, const CString &strNewAreaName);
+	///<param name="oCity">Референция към обект.</param>
+	///<param name="strNewAreaName">Стойност с която ще бъде подменена текущата намираща се в "szAREA_NAME".</param>
+	void ChangeCityAreaName(CITIES& oCity, const CString& strNewAreaName);
 
-	///<summary>Функция променяща 'POSTAL_CODE' стойността на обект чието текущо име е подадено като аргумент 'strCityName'.</summary>
-	/// <param name="strCityName">Текущото име на градът.</param>
-	/// <param name="nNewCityPosalCode">Нов пощенски код.</param>
-	/// <param name="oCitiesArray">Масив в който ще се търси обект по стоиността 'strCityName'.param>
+	///<summary>
+		///Функция променяща 'POSTAL_CODE' стойността на обект
+		///чието текущо име е подадено като аргумент 'strCityName'.
+	///</summary>
+	///<param name="strCityName">Текущото име на градът.</param>
+	///<param name="nNewCityPosalCode">Нов пощенски код.</param>
+	///<param name="oCitiesArray">Масив в който ще се търси обект по стоиността 'strCityName'.param>
 	void ChangeCityPosalCodeByCityName(const CString& strCityName, const int nNewCityPosalCode);
 	///<summary>Функция променяща 'POSTAL_CODE' стойността на обект подаден като аргумент 'pCity'.</summary>
-	/// <param name="оCity">Референция към обект.</param>
-	/// <param name="nNewPostalCode">Нов пощенски код.</param>
+	///<param name="оCity">Референция към обект.</param>
+	///<param name="nNewPostalCode">Нов пощенски код.</param>
 	void ChangeCityPostalCode(CITIES& оCity, const int nNewPostalCode);
 
 	///<summary>
 		/// Функция добавяща всички дефоутни елементи в подаден масив като
 		/// аргумент, пренаписана от базов клас 'BaseService'.
-	///  </summary>
+	///</summary>
 	/// <param name="citiesArray">Масив в който ще се добавят всички стойности.</param>
 	void AddDefaultElements() override;
 
-	///<summary>Функция принтираща информация за обект намиращ се на позицията на nIndex, пренаписана от базов клас 'BaseService'.</summary>
-	/// <param name="nIndex">Позиция в масива.</param>
-	/// <param name="oArray">Масив със стойности.</param> 
+	///<summary>
+		///Функция принтираща информация за обект намиращ
+		///се на позицията на nIndex, пренаписана от базов клас 'BaseService'.
+	///</summary>
+	///<param name="nIndex">Позиция в масива.</param>
+	///<param name="oArray">Масив със стойности.</param> 
 	void ShowElementInfoAtIndex(const int nIndex)override;
 };
 
