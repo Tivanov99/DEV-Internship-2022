f4e27a153a4ea053048520ce1c0f2f0b59c60c8d
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-14 14:10:55 +0300

    Added implementation to each method in CCitiesData.cpp

--- PhoneBook/CCitiesData.cpp
+++ PhoneBook/CCitiesData.cpp
@@ -6,24 +6,24 @@ CCitiesData::~CCitiesData() {};
 
 bool CCitiesData::SelectAll(CSelfClearingTypedPtrArray<CITIES>& oCitiesArray)
 {
-
+	m_CitiesTable.SelectAll(oCitiesArray);
 };
 bool CCitiesData::SelectWhereID(const long lID, CITIES& recCities)
 {
-
+	m_CitiesTable.SelectWhereID(lID, recCities);
 }
 
 bool CCitiesData::UpdateWhereID(const long lID, const CITIES& recCities)
 {
-
+	m_CitiesTable.UpdateWhereID(lID, recCities);
 }
 
 bool CCitiesData::Insert(const CITIES& recCities)
 {
-
+	m_CitiesTable.Insert(recCities);
 }
 
 bool CCitiesData::DeleteWhereID(const long lID)
 {
-
+	m_CitiesTable.DeleteWhereID(lID);
 }
