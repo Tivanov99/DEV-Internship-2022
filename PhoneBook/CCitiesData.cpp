fbe6f65ed4ba653c8af265ac2be92629d81e61f3
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-14 13:51:52 +0300

    Add inline method's to CCitiesData.cpp

--- PhoneBook/CCitiesData.cpp
+++ PhoneBook/CCitiesData.cpp
@@ -1,2 +1,29 @@
 #include "pch.h"
 #include "CCitiesData.h"
+
+CCitiesData::CCitiesData() {};
+CCitiesData::~CCitiesData() {};
+
+bool CCitiesData::SelectAll(CSelfClearingTypedPtrArray<CITIES>& oCitiesArray)
+{
+
+};
+bool CCitiesData::SelectWhereID(const long lID, CITIES& recCities)
+{
+
+}
+
+bool CCitiesData::UpdateWhereID(const long lID, const CITIES& recCities)
+{
+
+}
+
+bool CCitiesData::Insert(const CITIES& recCities)
+{
+
+}
+
+bool CCitiesData::DeleteWhereID(const long lID)
+{
+
+}
