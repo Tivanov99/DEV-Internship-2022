1caa5b591444b860d3ce3d2251fdef1207c30520
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-25 13:38:43 +0300

    Added other structures to Structures.h

--- PhoneBook/CPersonsDocument.h
+++ PhoneBook/CPersonsDocument.h
@@ -1,6 +1,35 @@
 #pragma once
-class CPersonsDocument
+class CPersonsDocument :public CDocument
 {
+protected: // create from serialization only
+	CPersonsDocument() noexcept;
+	DECLARE_DYNCREATE(CPersonsDocument)
 
+	// Overrides
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+	// Implementation
+public:
+	virtual ~CPersonsDocument();
+
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+	//Methods
+public:
+	const CSelfClearingTypedPtrArray< CITIES>& GetAllCities();
+	CITIES* GetCityById(long lID);
+	bool DeleteCityById(long lID);
+	bool UpdateCity(CITIES& recCity);
+	bool InsertCity(CITIES& recCity);
 };
 
--- PhoneBook/Structures.h
+++ PhoneBook/Structures.h
@@ -31,6 +31,78 @@ struct CITIES
 	}
 };
 
+#define FIRST_NAME_SIZE		32
+#define SECOND_NAME_SIZE	32
+#define LAST_NAME_SIZE		32
+#define ADDRESS_SIZE		256
+
+/// <summary>
+///  Дискова структура PERSONS предназначена за таблицата 'PERSONS'
+/// </summary>
+struct PERSONS {
+	/// <summary>Променлива съдържаща първичен ключ от колоната 'ID' в таблицата 'PERSONS'</summary>
+	long lID;
+	/// <summary>?</summary>
+	long lUPDATE_COUNTER;
+	/// <summary>Променлива съдържаща стойността от колона 'FIRST_NAME' в таблицата  'PERSONS'</summary>
+	TCHAR szFIRST_NAME[FIRST_NAME_SIZE];
+	/// <summary>Променлива съдържаща стойността от колона 'SECOND_NAME' в таблицата  'PERSONS'</summary>
+	char szSECOND_NAME[SECOND_NAME_SIZE];
+	/// <summary>Променлива съдържаща стойността от колоната 'LAST_NAME' в таблицата  'PERSONS'</summary>
+	TCHAR szLAST_NAME[LAST_NAME_SIZE];
+	/// <summary>Променлива съдържаща вторичен ключ (от колона 'CITY_ID' в таблицата  'PERSONS')
+	/// <summary>сочещ към таблицата 'CITIES' и колона 'ID'</summary>
+	long lCITY_ID;
+	/// <summary>Променлива съдържаща стойността от колоната 'ADDRESS' в таблицата  'PERSONS'</summary>
+	TCHAR szADDRESS[ADDRESS_SIZE];
+
+	PERSONS()
+	{
+		SecureZeroMemory(this, sizeof(*this));
+	}
+};
+
+#define PHONE_NUMBER_SIZE	16
+
+/// <summary>
+///  Дискова структура 'PHONE_NUMBERS' предназначена за таблицата 'PHONE_NUMBERS'
+/// </summary>
+struct PHONE_NUMBERS {
+	/// <summary>Променлива съдържаща първичен ключ от колоната 'ID' в таблицата 'PHONE_NUMBERS'</summary>
+	long lID;
+	/// <summary>?</summary>
+	long lUPDATE_COUNTER;
+	/// <summary>
+		/// Променлива съдържаща вторичен ключ (от колона 'PHONE_TYPE_ID' в таблицата 'PHONE_TYPES')
+		/// сочещ към колоната 'ID' в таблицата 'PHONE_NUMBERS'</summary>;
+	/// </summary>
+	long lPHONE_TYPE_ID;
+	/// <summary>Променлива съдържаща стойността от колоната 'PHONE_NUMBER' в таблицата  'PHONE_NUMBERS'</summary>
+	TCHAR szPHONE_NUMBER[PHONE_NUMBER_SIZE];
+
+	PHONE_NUMBERS()
+	{
+		SecureZeroMemory(this, sizeof(*this));
+	}
+};
+
+#define PHONE_TYPE_SIZE	16
+/// <summary>
+///  Дискова структура PHONE_TYPES предназначена за таблицата 'PHONE_TYPES'
+/// </summary>
+struct PHONE_TYPES {
+	/// <summary>Променлива съдържаща първичен ключ от колоната 'ID' в таблицата 'PHONE_TYPES'</summary>
+	long lID;
+	/// <summary>?</summary>
+	long lUPDATE_COUNTER;
+	/// <summary>Променлива съдържаща стойността от колоната 'PHONE_TYPE' в таблицата  'PHONE_TYPES'</summary>
+	TCHAR szPHONE_TYPE[PHONE_TYPE_SIZE];
+	PHONE_TYPES()
+	{
+		SecureZeroMemory(this, sizeof(*this));
+	};
+};
+
 //typedef CSelfClearingTypedPtrArray<CITIES> CCitiesArray;
 enum ContextMenuOperations
 {
