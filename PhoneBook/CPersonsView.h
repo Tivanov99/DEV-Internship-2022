51fd71d3bfc5207400dfa9dcf72c1dea739cd3de
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-25 13:34:32 +0300

    Added CPersonsDocument

--- PhoneBook/CPersonsView.h
+++ PhoneBook/CPersonsView.h
@@ -1,6 +1,20 @@
 #pragma once
+#include "CPersonsDocument.h"
+
 class CPersonsView :public CListView
 {
+protected: // create from serialization only
+	CPersonsView() noexcept;
+	DECLARE_DYNCREATE(CPersonsView)
 
+	// Attributes
+public:
+	/// <summary>
+	/// Връща документ чрез който се достъпват информацията от базата данни.
+	/// </summary>
+	/// <returns>Указател на документа.</returns>
+	CPersonsDocument* GetDocument() const;
+
+	//Methods
 };
 
