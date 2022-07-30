451b8c348e1204e33860f711555c9018d8c7e919
Tivanov99 <trifon.ivanov21@gmail.com>
2022-07-30 20:30:11 +0300

    Add phone numbers accessor

--- PhoneBook/CPhoneNumbersTable.h
+++ PhoneBook/CPhoneNumbersTable.h
@@ -2,6 +2,30 @@
 #include "CBaseTable.h"
 
 
+#define NoneModifyColumnCode 0
+#define ModifyColumnCode 1
+class CPhoneNumberAccessor
+{
+protected:
+	CPhoneNumberAccessor()
+	{};
+	~CPhoneNumberAccessor() {
+	};
+	PHONE_NUMBERS m_recPhoneNumber;
+
+	BEGIN_ACCESSOR_MAP(CPhoneNumberAccessor, 2)
+		BEGIN_ACCESSOR(NoneModifyColumnCode, true)
+		COLUMN_ENTRY(1, m_recPhoneNumber.lID)
+		END_ACCESSOR()
+
+		BEGIN_ACCESSOR(ModifyColumnCode, true)
+		COLUMN_ENTRY(2, m_recPhoneNumber.lUpdateCounter)
+		COLUMN_ENTRY(3, m_recPhoneNumber.lPHONE_TYPE_ID)
+		COLUMN_ENTRY(4, m_recPhoneNumber.szPHONE_NUMBER)
+		END_ACCESSOR()
+	END_ACCESSOR_MAP()
+};
+
 #pragma once
 class CPhoneNumbersTable : public CBaseTable<PHONE_NUMBERS>
 {
