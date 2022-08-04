04a5012064f26c7694de8a3c1e6b7c219b8c7728
Tivanov99 <trifon.ivanov21@gmail.com>
2022-08-04 15:56:19 +0300

    Added separate comments

--- PhoneBook/CPhoneNumbersDialog.h
+++ PhoneBook/CPhoneNumbersDialog.h
@@ -8,6 +8,8 @@ class CPhoneNumbersDialog : public CDialog
 {
 	DECLARE_DYNAMIC(CPhoneNumbersDialog)
 
+	//Constructor/Destructor
+	//-----------------
 public:
 	CPhoneNumbersDialog(CWnd* pParent = nullptr);   // standard constructor
 	virtual ~CPhoneNumbersDialog();
@@ -17,10 +19,21 @@ public:
 	enum { IDD = IDD_PHONE_NUMBERS_DIALOG };
 #endif
 
+	//Methods
+	//-----------------
+private:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+
+	//Overrides
+	//-----------------
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
 	DECLARE_MESSAGE_MAP()
+
+	//Members
+	//-----------------
 private:
 	CEdit m_edbPhoneNumber;
 
