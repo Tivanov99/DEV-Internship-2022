67da9e69cbbb3ccacec2bbb4ab769c379f80ecb0
Tivanov99 <trifon.ivanov21@gmail.com>
2022-08-11 12:42:16 +0300

    Added implementation to CPersonsFull class

--- PhoneBook/CPersonsFull.cpp
+++ PhoneBook/CPersonsFull.cpp
@@ -1,2 +1,18 @@
 #include "pch.h"
 #include "CPersonsFull.h"
+
+CPersonsFull:: CPersonsFull(PERSONS& recPerson, CPhoneNumbersArray& oPhoneNumbersArray)
+	:m_recPerson(recPerson), m_PhoneNumbersArray(oPhoneNumbersArray)
+{}
+
+CPersonsFull:: ~CPersonsFull() {};
+
+PERSONS& CPersonsFull::GetPerson()
+{
+	return m_recPerson;
+}
+
+CPhoneNumbersArray& CPersonsFull::GetPhoneNumbers()
+{
+	return m_PhoneNumbersArray;
+}
--- PhoneBook/CPersonsFull.h
+++ PhoneBook/CPersonsFull.h
@@ -2,8 +2,21 @@
 #include "Structures.h"
 class CPersonsFull
 {
+public:
+	//Constructor/Destrucotr
+	//-----------------------
 	CPersonsFull(PERSONS& recPerson, CPhoneNumbersArray& oPhoneNumbersArray);
 	~CPersonsFull();
 
+	//Methods
+	//--------------
+public:
+	PERSONS& GetPerson();
+	CPhoneNumbersArray& GetPhoneNumbers();
+	//Members:
+	//------------------
+private:
+	PERSONS& m_recPerson;
+	CPhoneNumbersArray& m_PhoneNumbersArray;
 };
 
